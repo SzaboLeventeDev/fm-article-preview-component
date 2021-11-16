6cb9fbe7d6453ecc73c8005884f03b2df80dfb1d
Szabó Levente <szabo.levente.dev@gmail.com>
2021-11-16 13:56:25 +0100

    responsive design done

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
+    var authorIsHidden = false;
+    var author = document.getElementsByClassName("author");
     $('.share-icon').on('click', () =>{
-        $('.hide').toggle();
+        var screenWidth = window.innerWidth;
+        
+        console.log(window.innerWidth);
+       /*  if (authorIsHidden == false && screenWidth <= 600) {
+            console.log("mobil felbontás");
+            $('.left').css('opacity', '0');
+            $('.hide').css('opacity', '1');
+            authorIsHidden = true;
+            console.log(authorIsHidden); 
+            $('.hide').toggle();
+        }else if(authorIsHidden == true && screenWidth <= 600){
+            console.log("mobil felbontás");
+            $('.left').css('opacity', '1');
+            $('.hide').css('opacity', '0');
+            authorIsHidden = false;
+            $('.hide').toggle();
+        }else{
+            console.log('nem mobilos felbontás');
+            $('.hide').toggle();
+        } */
+
+        if (screenWidth <= 375) {
+            $('.hide').toggle('.fade_in');
+            $('.left').toggle('.fade_out');
+            $('.right').css('width', '100%');
+
+        }
+        else{
+            $('.hide').toggle('.fade_in');
+        }
+        
     })
 })
